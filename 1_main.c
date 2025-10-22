50d381840130d3d732ee9f2b57218c848be40169
Ztttyy <3447508407@qq.com>
2025-10-22 23:56:02 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -2,8 +2,16 @@
 //
 //
 #include <stdio.h>
-int main()
-{
-    printf("Hello World");
+
+int main(){
+    float C=0;
+    printf("请输入一个摄氏温度:");
+    scanf("%f",&C);
+
+    float H=C*1.0*9/5+32;
+
+    printf("您输入的摄氏温度为:%.1f\n",C);
+    printf("对应的华氏温度为:%.1f\n",H);
+
     return 0;
 }
