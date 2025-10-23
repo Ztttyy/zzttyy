b112a71d891766dfe683e6d98f041646f0a20990
Ztttyy <3447508407@qq.com>
2025-10-23 21:05:43 +0800

    Create 4_main.c

---
+++ 4_main.c
@@ -0,0 +1,20 @@
+//202510302124
+//3447508407@qq.com
+//谢金恩
+#include <stdio.h>
+
+int main(){
+    int a,b,c=0;
+
+    printf("请输入三角形的三个边长：\n");
+    scanf("%d %d %d",&a,&b,&c);
+
+    if(a+b>c && a+c>b && b+c>a){
+        printf("可以组成三角形!\n");
+    }else{
+        printf("不能构成三角形!\n");
+    }
+    return 0;
+
+
+}
